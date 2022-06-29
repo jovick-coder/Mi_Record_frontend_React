75206213360a572476d500e1af189eb7463a5144
Jovick-coder <victorjosiahm3@gmail.com>
2022-06-29 10:30:35 +0100

    routed basic pages auto navigate to homepahe if logined in

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Navigate, Route, Routes, useNavigate } from "react-router-dom";
+import Dashboard from "./Pages/dashboard/Dashboard";
+import LandingPage from "./Pages/landing_page/LandingPage";
+import LoginPage from "./Pages/login_page/LoginPage";
+import RegisterPage from "./Pages/register_page/RegisterPage";
+import HomePage from "./Pages/home/HomePage";
+import { useContext, useEffect } from "react";
+import { UserContext } from "./context/userContext";
 
 function App() {
+  const { loggedIn } = useContext(UserContext);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (loggedIn) {
+      navigate("/dashboard/home");
+    }
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route exact path="/" element={<LandingPage />} />
+        <Route exact path="/login" element={<LoginPage />} />
+        <Route exact path="/register" element={<RegisterPage />} />
+        <Route path="/dashboard" element={<Dashboard />}>
+          <Route path="home" element={<HomePage />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
