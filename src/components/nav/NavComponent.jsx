70190e90bbdb7c5d09e4e9cbfb4fa5156cf68c67
Jovick-coder <victorjosiahm3@gmail.com>
2022-06-29 12:05:22 +0100

    tmp nav

---
+++ src/components/nav/NavComponent.jsx
@@ -0,0 +1,35 @@
+import React, { useContext } from "react";
+import {
+  BsArrowLeftSquareFill,
+  BsArrowRightSquareFill,
+  BsFillDoorOpenFill,
+} from "react-icons/bs";
+import { Link } from "react-router-dom";
+import Logo from "../../components/logo/Logo";
+
+function NavComponent() {
+  return (
+    <>
+      {" "}
+      <nav>
+        <Logo /> Mi_Records
+        <ul>
+          <li>
+            {" "}
+            <Link to={"/"}> Home</Link>
+          </li>
+          <li>
+            {" "}
+            <Link to={"/login"}>Login</Link>
+          </li>
+          <li>
+            {" "}
+            <Link to={"/register"}>Sign in</Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
+
+export default NavComponent;
