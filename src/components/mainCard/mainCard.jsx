72f2b297a8e88db903a4d4f198f91c3bda7c9832
Jovick-coder <victorjosiahm3@gmail.com>
2022-06-30 06:53:50 +0100

    mad a reusabe component for main card

---
+++ src/components/mainCard/mainCard.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./mainCard.css";
+
+function MainCard({ children }) {
+  return <div className="main-card text-center">{children}</div>;
+}
+
+export default MainCard;
