07f9bb8148864c2ad4ea786d3a817f6a1f36062b
Jovick-coder <victorjosiahm3@gmail.com>
2022-06-30 06:51:25 +0100

    nav links list

--- src/components/NavBar/dashboardNavList.js
+++ src/components/NavBar/dashboardNavList.js
@@ -1,98 +1,22 @@
-import {
-  // BsAward,
-  BsBellFill,
-  BsCoin,
-  BsFillHddStackFill,
-  BsHouseFill,
-  BsPeople,
-  BsPerson,
-  // BsPersonBadge,
-  BsPersonBoundingBox,
-  BsPersonCheck,
-  BsPersonPlus,
-  BsSave,
-  // BsPersonX,
-  BsSimFill,
-} from "react-icons/bs";
-import { AiFillApi } from "react-icons/ai";
+import { BsPerson } from "react-icons/bs";
+import { AiFillDashboard } from "react-icons/ai";
+import { FaCoins } from "react-icons/fa";
 
 export const userNavLinkObject = [
   {
-    name: "Home",
-    icon: <BsHouseFill />,
+    name: "Dashboard",
+    icon: <AiFillDashboard />,
     path: "/dashboard/home",
-    userPrivilege: 1,
   },
   {
     name: "profile",
     icon: <BsPerson />,
     path: "/dashboard/#",
-    userPrivilege: 1,
   },
-  // {
-  //   name: "History",
-  //   icon: <BsFillHddStackFill />,
-  //   path: "/dashboard/#",
-  //   userPrivilege: 1,
-  // },
-  // {
-  //   name: "Notification",
-  //   icon: <BsBellFill />,
-  //   path: "/dashboard/#",
-  //   userPrivilege: 1,
-  // },
-  // {
-  //   name: "Found Account",
-  //   icon: <BsCoin />,
-  //   path: "/dashboard/#",
-  //   userPrivilege: 1,
-  // },
-  // {
-  //   name: "Buy Subscriptions",
-  //   icon: <BsSimFill />,
-  //   path: "/dashboard/#",
-  //   userPrivilege: 1,
-  // },
-  // {
-  //   name: "Customers List",
-  //   icon: <BsPersonCheck />,
-  //   path: "/dashboard/#",
-  //   userPrivilege: 2,
-  // },
-  // {
-  //   name: "Users List",
-  //   icon: <BsPeople />,
-  //   path: "/dashboard/#",
-  //   userPrivilege: 3,
-  // },
-  // {
-  //   name: "Resellers List",
-  //   icon: <BsPersonPlus />,
-  //   path: "/dashboard/#",
-  //   userPrivilege: 3,
-  // },
-  // {
-  //   name: "Admins List",
-  //   icon: <BsPersonBoundingBox />,
-  //   path: "/dashboard/#",
-  //   userPrivilege: 4,
-  // },
-  // {
-  //   name: "API Dashboard",
-  //   icon: <AiFillApi />,
-  //   path: "/dashboard/#",
-  //   userPrivilege: 4,
-  // },
-  // {
-  //   name: "API History",
-  //   icon: <AiFillApi />,
-  //   path: "/dashboard/#",
-  //   userPrivilege: 4,
-  // },
-  // {
-  //   name: "Confirmation",
-  //   icon: <BsSave />,
-  //   path: "/dashboard/#",
-  //   userPrivilege: 4,
-  // },
+
+  {
+    name: "Fiances",
+    icon: <FaCoins />,
+    path: "/dashboard/#",
+  },
 ];
