964f73d5e4520e45c1448ef7dfa200d56318e2b5
Jovick-coder <victorjosiahm3@gmail.com>
2022-06-30 16:12:48 +0100

    variable rename

--- src/components/anyChat/AnyChat.jsx
+++ src/components/anyChat/AnyChat.jsx
@@ -1,23 +1,24 @@
 import React from "react";
 import AnyChart from "anychart-react/dist/anychart-react";
+import "./AnyChart.css";
 
 export function ProjectReportChat({ type, data, title }) {
-  const complexSettings = {
+  const otherAttributes = {
     minWidth: "240px",
     width: "50%",
     height: "100%",
   };
   return (
-    <AnyChart type={type} data={data} title={title} {...complexSettings} />
+    <AnyChart type={type} data={data} title={title} {...otherAttributes} />
   );
 }
 export function FinanceReportChat({ type, data, title }) {
-  const complexSettings = {
+  const otherAttributes = {
     minWidth: "240px",
     width: "50%",
     height: "100%",
   };
   return (
-    <AnyChart type={type} data={data} title={title} {...complexSettings} />
+    <AnyChart type={type} data={data} title={title} {...otherAttributes} />
   );
 }
