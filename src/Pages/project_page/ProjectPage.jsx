4343a78036e9aa9bce2e5e3e7309b420b4f20565
Jovick-coder <victorjosiahm3@gmail.com>
2022-07-11 12:00:32 +0100

    created project form component

--- src/Pages/project_page/ProjectPage.jsx
+++ src/Pages/project_page/ProjectPage.jsx
@@ -33,110 +33,7 @@ function ProjectPage() {
         <div className="row justify-content-around project-form-div-row">
           <div className="col-sm-8">
             <div className="form-div dashboard-form">
-              <form action="" id="project-form">
-                <div className="input-div">
-                  <label htmlFor="project-name">
-                    <i className="fas fa-user-tag"></i>
-                  </label>
-                  <input
-                    type="text"
-                    id="project-name"
-                    placeholder="Project Name"
-                  />
-                </div>
-                <div className="textarea-div">
-                  <label htmlFor="discretion" className="text-arear-lable">
-                    <i className="fas fa-sticky-note"></i>
-                  </label>
-                  <textarea
-                    type="'text"
-                    name=""
-                    id="discretion"
-                    cols="30"
-                    rows="3"
-                    placeholder="Project Discretion"
-                  ></textarea>
-                </div>
-                {/* <!-- <sub className="m-0">Task Dead Line</sub> --> */}
-                <div className="input-div mt-0">
-                  <label htmlFor="upload-date">
-                    <i className="fas fa-calendar-day"></i>
-                  </label>
-                  <input
-                    type="datetime-local"
-                    id="upload-date"
-                    placeholder="Dead Line"
-                  />
-                </div>
-                <div className="input-div progress-div">
-                  <label>
-                    <input
-                      type="radio"
-                      className="progress-checkbox"
-                      name="progress"
-                      id="progress-Pending"
-                      value="Pending"
-                    />
-                    Pending
-                  </label>
-                  <label>
-                    <input
-                      type="radio"
-                      className="progress-checkbox"
-                      name="progress"
-                      id="progress-InProgress"
-                      value="InProgress"
-                    />
-                    InProgress
-                  </label>
-                  <label>
-                    <input
-                      type="radio"
-                      className="progress-checkbox"
-                      name="progress"
-                      id="progress-Completed"
-                      value="Completed"
-                    />
-                    Completed
-                  </label>
-                </div>
-                <div className="input-div">
-                  <label htmlFor="git-link">
-                    <i className="fab fa-github"></i>
-                  </label>
-                  <input type="text" id="git-link" placeholder="GitHub Link" />
-                </div>
-                <div className="input-div">
-                  <label htmlFor="live-link">
-                    <i className="fas fa-link"></i>
-                  </label>
-                  <input type="text" id="live-link" placeholder="Live Link" />
-                </div>
-                <div className="input-div image-upload" id="dropbox">
-                  <label htmlFor="project-image">
-                    <i className="fas fa-image">
-                      <p>
-                        Click to Upload picture
-                        <br />
-                        OR
-                        <br />
-                        Drag & Drop
-                      </p>
-                    </i>
-                  </label>
-                  <input
-                    type="file"
-                    id="project-image"
-                    placeholder="project-image"
-                  />
-                </div>
-                <div>
-                  <button type="submit" className="form-btn">
-                    <i className="fas fa-paper-plane"></i>
-                    Add Task
-                  </button>
-                </div>
-              </form>
+              <ProjectFormComponent />
             </div>
           </div>
         </div>
@@ -155,3 +52,100 @@ function ProjectPage() {
 }
 
 export default ProjectPage;
+
+export function ProjectFormComponent() {
+  return (
+    <form action="" id="project-form">
+      <div className="input-div">
+        <label htmlFor="project-name">
+          <i className="fas fa-user-tag"></i>
+        </label>
+        <input type="text" id="project-name" placeholder="Project Name" />
+      </div>
+      <div className="textarea-div">
+        <label htmlFor="discretion" className="text-arear-lable">
+          <i className="fas fa-sticky-note"></i>
+        </label>
+        <textarea
+          type="'text"
+          name=""
+          id="discretion"
+          cols="30"
+          rows="3"
+          placeholder="Project Discretion"
+        ></textarea>
+      </div>
+      {/* <!-- <sub className="m-0">Task Dead Line</sub> --> */}
+      <div className="input-div mt-0">
+        <label htmlFor="upload-date">
+          <i className="fas fa-calendar-day"></i>
+        </label>
+        <input type="datetime-local" id="upload-date" placeholder="Dead Line" />
+      </div>
+      <div className="input-div progress-div">
+        <label>
+          <input
+            type="radio"
+            className="progress-checkbox"
+            name="progress"
+            id="progress-Pending"
+            value="Pending"
+          />
+          Pending
+        </label>
+        <label>
+          <input
+            type="radio"
+            className="progress-checkbox"
+            name="progress"
+            id="progress-InProgress"
+            value="InProgress"
+          />
+          InProgress
+        </label>
+        <label>
+          <input
+            type="radio"
+            className="progress-checkbox"
+            name="progress"
+            id="progress-Completed"
+            value="Completed"
+          />
+          Completed
+        </label>
+      </div>
+      <div className="input-div">
+        <label htmlFor="git-link">
+          <i className="fab fa-github"></i>
+        </label>
+        <input type="text" id="git-link" placeholder="GitHub Link" />
+      </div>
+      <div className="input-div">
+        <label htmlFor="live-link">
+          <i className="fas fa-link"></i>
+        </label>
+        <input type="text" id="live-link" placeholder="Live Link" />
+      </div>
+      <div className="input-div image-upload" id="dropbox">
+        <label htmlFor="project-image">
+          <i className="fas fa-image">
+            <p>
+              Click to Upload picture
+              <br />
+              OR
+              <br />
+              Drag & Drop
+            </p>
+          </i>
+        </label>
+        <input type="file" id="project-image" placeholder="project-image" />
+      </div>
+      <div>
+        <button type="submit" className="form-btn">
+          <i className="fas fa-paper-plane"></i>
+          Add Task
+        </button>
+      </div>
+    </form>
+  );
+}
