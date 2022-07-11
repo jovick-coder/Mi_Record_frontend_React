acb1b470fe5b47b514b0d20089d86f9077a7ef82
Jovick-coder <victorjosiahm3@gmail.com>
2022-07-11 18:23:23 +0100

    addad project tasl form

--- src/Pages/project_page/ProjectPage.jsx
+++ src/Pages/project_page/ProjectPage.jsx
@@ -1,5 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { FaBriefcase, FaFolderPlus, FaPlus, FaSearch } from "react-icons/fa";
+import {
+  FaBriefcase,
+  FaCalendarDay,
+  FaCheckDouble,
+  FaFolderPlus,
+  FaPaperPlane,
+  FaPlus,
+  FaSearch,
+  FaStickyNote,
+  FaTasks,
+  FaTrash,
+  FaUserTag,
+} from "react-icons/fa";
 import "./ProjectPage.css";
 import { nanoid } from "nanoid";
 import imageAlt from "../../assets/images/alt.jpg";
@@ -386,6 +398,124 @@ export function ViewProject({ project }) {
           {liveLink}
         </li>
       </ul>
+      <div className="row">
+        <div className="col-md-6">
+          <div className="form-div dashboard-form">
+            <form action="">
+              <div className="input-div">
+                <label htmlFor="task-name">
+                  <FaUserTag />
+                </label>
+                <input type="text" id="task-name" placeholder="Task Name" />
+              </div>
+              <div className="textarea-div">
+                <label htmlFor="password" className="text-areal-label">
+                  <FaStickyNote />
+                </label>
+                <textarea
+                  name=""
+                  // id="discribtion"
+                  cols="30"
+                  rows="3"
+                  placeholder="Task Discretion"
+                ></textarea>
+              </div>
+              <sub className="m-0">Task Dead Line</sub>
+              <div className="input-div mt-0">
+                <label htmlFor="task-name">
+                  <FaCalendarDay />
+                </label>
+                <input
+                  type="datetime-local"
+                  id="task-name"
+                  placeholder="Dead Line"
+                />
+              </div>
+              <div>
+                <button className="form-btn">
+                  <FaPaperPlane />
+                  Add Task
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div className="col-md-6">
+          <b className="d-block my-2">
+            {" "}
+            <FaTasks />
+            Tasks
+          </b>
+          <div className="reminder-task-div">
+            <ul>
+              <li className="task-list">
+                <b>Task Name</b>
+                <p>Task Discretion</p>
+                <span className="d-flex justify-content-between">
+                  <i>
+                    <b>set :</b> Date
+                  </i>
+                  <i>
+                    <b>dead line :</b> Date
+                  </i>
+                </span>
+                <div className="icon-div">
+                  <FaCheckDouble />
+                  <FaTrash />
+                </div>
+              </li>
+              <li className="task-list">
+                <b>Task Name</b>
+                <p>Task Discretion</p>
+                <span className="d-flex justify-content-between">
+                  <i>
+                    <b>set :</b> Date
+                  </i>
+                  <i>
+                    <b>dead line :</b> Date
+                  </i>
+                </span>
+                <div className="icon-div">
+                  <FaCheckDouble />
+                  <FaTrash />
+                </div>
+              </li>
+              <li className="task-list">
+                <b>Task Name</b>
+                <p>Task Discretion</p>
+                <span className="d-flex justify-content-between">
+                  <i>
+                    <b>set :</b> Date
+                  </i>
+                  <i>
+                    <b>dead line :</b> Date
+                  </i>
+                </span>
+                <div className="icon-div">
+                  <FaCheckDouble />
+                  <FaTrash />
+                </div>
+              </li>
+              <li className="task-list">
+                <b>Task Name</b>
+                <p>Task Discretion</p>
+                <span className="d-flex justify-content-between">
+                  <i>
+                    <b>set :</b> Date
+                  </i>
+                  <i>
+                    <b>dead line :</b> Date
+                  </i>
+                </span>
+                <div className="icon-div">
+                  <FaCheckDouble />
+                  <FaTrash />
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
