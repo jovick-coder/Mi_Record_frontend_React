629b4ecd86db9aa3db3e980b651d86e2f11e8214
Jovick-coder <victorjosiahm3@gmail.com>
2022-07-12 12:01:50 +0100

    CountDownFunction

--- src/components/countDownComponent/CountDownComponent.jsx
+++ src/components/countDownComponent/CountDownComponent.jsx
@@ -71,3 +71,51 @@ export default function CountDownComponent() {
     </div>
   );
 }
+
+export function CountDownFunction() {
+  const second = 1000,
+    minute = second * 60,
+    hour = minute * 60,
+    day = hour * 24;
+
+  const [countDown, setCountDown] = useState({
+    Month: "jul",
+    Day: "30",
+    Year: "2022",
+    Time: "00:00:00",
+  });
+
+  const [calculatedDistance, setCalculatedDistance] = useState(0);
+  const [countedDay, setCountedDay] = useState(0);
+  const [countedHours, setCountedHours] = useState(0);
+  const [countedMinutes, setCountedMinutes] = useState(0);
+  const [countedSeconds, setCountedSeconds] = useState(0);
+
+  useEffect(
+    function () {
+      setCountedDay(Math.floor(calculatedDistance / day));
+      setCountedHours(Math.floor((calculatedDistance % day) / hour));
+      setCountedMinutes(Math.floor((calculatedDistance % hour) / minute));
+      setCountedSeconds(Math.floor((calculatedDistance % minute) / second));
+    },
+    [calculatedDistance]
+  );
+
+  useEffect(() => {
+    let countTill = `${countDown.Month} ${countDown.Day} , ${countDown.Year} ${countDown.Time}`;
+    let countDownDate = new Date(countTill).getTime();
+    const interval = setInterval(() => {
+      let now = new Date().getTime(),
+        distance = countDownDate - now;
+      setCalculatedDistance(distance);
+    }, 0);
+    return () => clearInterval(interval);
+  }, []);
+
+  return {
+    day: Math.floor(calculatedDistance / day),
+    hour: Math.floor((calculatedDistance % day) / hour),
+    minute: Math.floor((calculatedDistance % hour) / minute),
+    second: Math.floor((calculatedDistance % minute) / second),
+  };
+}
