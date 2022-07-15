bbbf87bce870239027c2a1b220043a6ce0b0d96a
Jovick-coder <victorjosiahm3@gmail.com>
2022-07-15 19:26:32 +0100

    add link component

--- src/Pages/social_links_page/SocialLinksPage.jsx
+++ src/Pages/social_links_page/SocialLinksPage.jsx
@@ -1,10 +1,12 @@
+import { nanoid } from "nanoid";
 import React, { useContext, useState } from "react";
-import { FaEdit, FaTimesCircle, FaTrash, FaXing } from "react-icons/fa";
+import { FaEdit, FaLink, FaSave, FaTimesCircle, FaTrash } from "react-icons/fa";
 import { SocialLinksContext } from "../../context/SocialLinksContext";
 import "./SocialLinksPage.css";
 
 export default function SocialLinksPage() {
   const { socialLinks, setSocialLinks } = useContext(SocialLinksContext);
+  const [addNewLink, setAddNewLink] = useState(false);
   // console.log(socialLinks);
   return (
     <>
@@ -33,6 +35,17 @@ export default function SocialLinksPage() {
             />
           );
         })}
+
+        {addNewLink ? (
+          <AddSocialLinkForm setAddNewLink={setAddNewLink} />
+        ) : (
+          <button
+            className="fancy-btn"
+            onClick={() => setAddNewLink(!addNewLink)}
+          >
+            Add Link
+          </button>
+        )}
       </div>
     </>
   );
@@ -150,6 +163,75 @@ export const LinkDiv = ({ social, socialLinks, setSocialLinks }) => {
             </div>
           </>
         )}
+      </div>
+    </>
+  );
+};
+
+export const AddSocialLinkForm = ({ setAddNewLink }) => {
+  const { socialLinks, setSocialLinks } = useContext(SocialLinksContext);
+  const [name, setName] = useState("");
+  const [link, setLink] = useState("");
+  function saveNewLink() {
+    if (name === "") return console.log("name is empty");
+    if (link === "") return console.log("link is empty");
+
+    const newLinkObject = {
+      linkId: nanoid(),
+      name: name,
+      icon: <FaLink />,
+      link: link,
+    };
+
+    setSocialLinks([...socialLinks, newLinkObject]);
+    setAddNewLink(false);
+  }
+  return (
+    <>
+      <div className="link-div">
+        <div className="text-div edit-form">
+          <div className="group">
+            <div className="icon">
+              <FaLink />
+              {/* <feature>Make a drop down of icons so one can select log for the social link <feature> */}
+              {/* <select className="form-select h-100">
+                    <option value="icon">{icon}</option>
+                  </select> */}
+            </div>
+            <div className="name">
+              <input
+                value={name}
+                className="form-control w-100"
+                onChange={(e) => {
+                  setName(e.currentTarget.value);
+                }}
+              />
+            </div>
+          </div>
+          <div className="link">
+            <input
+              value={link}
+              className="form-control w-100"
+              onChange={(e) => setLink(e.currentTarget.value)}
+            />
+          </div>
+        </div>
+        <div className="icons-div">
+          <div className="edit" onClick={() => saveNewLink()}>
+            <FaSave />
+          </div>
+          <div className="delete" onClick={() => setAddNewLink(false)}>
+            <FaTimesCircle />
+          </div>
+        </div>
+        {/* <div className="icons-div">
+          <div className="m-auto" onClick={() => setAddNewLink(false)}>
+            <FaSave />
+          </div>
+          <div className="m-auto" onClick={() => setAddNewLink(false)}>
+            <FaTimesCircle />
+          </div>
+        </div> */}
       </div>
     </>
   );
