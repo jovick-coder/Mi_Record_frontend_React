ce00e3e53e519fcf44a6acb93628cea80b5bd30c
Jovick-coder <victorjosiahm3@gmail.com>
2022-07-16 13:27:52 +0100

    create an arrey of object for todos map todos to UI

--- src/components/todoComponent/TodoComponent.jsx
+++ src/components/todoComponent/TodoComponent.jsx
@@ -1,60 +1,57 @@
-import React from "react";
+import { nanoid } from "nanoid";
+import React, { useState } from "react";
 import { FaPaperPlane, FaTrash } from "react-icons/fa";
 
 export default function TodoComponent() {
+  const [todoList, setTodoList] = useState([
+    {
+      todoId: nanoid(),
+      todo: "ToDo list functionality",
+      done: true,
+    },
+    {
+      todoId: nanoid(),
+      todo: "  Design other pages",
+      done: false,
+    },
+    {
+      todoId: nanoid(),
+      todo: "   Learn Node Js",
+      done: false,
+    },
+    {
+      todoId: nanoid(),
+      todo: "  Learn Mongo",
+      done: false,
+    },
+    {
+      todoId: nanoid(),
+      todo: "  Develope the Backend",
+      done: false,
+    },
+  ]);
   return (
     <div className="todo-div">
       <ol className="list-item" id="todo-list">
-        <li className="completed">
-          <span className="form-check form-check-flat">
-            <label className="done-check">
-              ToDo list functionality
-              <input type="checkbox" className="checkbox" checked />
-              <span className="checkmark"></span>
-            </label>
-          </span>
-          <FaTrash className="my-auto trash" />
-        </li>
-        <li>
-          <span className="form-check form-check-flat">
-            <label className="done-check">
-              Design other pages
-              <input type="checkbox" className="checkbox" />
-              <span className="checkmark"></span>
-            </label>
-          </span>
-          <FaTrash className="my-auto trash" />
-        </li>
-        <li>
-          <span className="form-check form-check-flat">
-            <label className="done-check">
-              Learn Node Js
-              <input type="checkbox" className="checkbox" />
-              <span className="checkmark"></span>
-            </label>
-          </span>
-          <FaTrash className="my-auto trash" />
-        </li>
-        <li>
-          <span className="form-check form-check-flat">
-            <label className="done-check">
-              Learn Mongo
-              <input type="checkbox" className="checkbox" />
-              <span className="checkmark"></span>
-            </label>
-          </span>
-          <FaTrash className="my-auto trash" />
-        </li>
-        <li>
-          <span className="form-check form-check-flat">
-            <label className="done-check">
-              Develope the Backend
-              <input type="checkbox" className="checkbox" />
-              <span className="checkmark"></span>
-            </label>
-          </span>
-          <FaTrash className="my-auto trash" />
-        </li>
+        {todoList.map((todoItem) => {
+          const { todoId, todo, done } = todoItem;
+          return (
+            <li className={done ? "completed" : ""} key={todoId}>
+              <span className="form-check form-check-flat">
+                <label className="done-check">
+                  {todo}
+                  <input
+                    type="checkbox"
+                    className="checkbox"
+                    checked={done ? true : false}
+                  />
+                  <span className="checkmark"></span>
+                </label>
+              </span>
+              <FaTrash className="my-auto trash" />
+            </li>
+          );
+        })}
       </ol>
       <TodoFormComponent />
     </div>
