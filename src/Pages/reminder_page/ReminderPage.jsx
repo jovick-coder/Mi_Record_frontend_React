09018d6fefc44dc1eea196a8053feb78c42740f9
Jovick-coder <victorjosiahm3@gmail.com>
2022-07-20 14:25:24 +0100

    created TaskFormComponent fro task form and MapTaskComponent to map task from the task array and toggleTaskDoneFunction functioln to toggel task done

--- src/Pages/reminder_page/ReminderPage.jsx
+++ src/Pages/reminder_page/ReminderPage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import "./ReminderPage.css";
 import {
   FaCalendarAlt,
@@ -16,8 +16,52 @@ import {
 import CountDownComponent from "../../components/countDownComponent/CountDownComponent";
 import DataAndTimeComponent from "../../components/dateAndTime/DataAndTimeComponent";
 import { ReminderFormComponent } from "../../components/reminderForm/ReminderFormComponent";
+import { nanoid } from "nanoid";
+import { UserContext } from "../../context/userContext";
 
 function ReminderPage() {
+  const [taskList, setTaskList] = useState([
+    {
+      taskId: nanoid(),
+      name: "Task 1 Name",
+      description: "Task 1 Description",
+      setDate: " 7/13/2022",
+      deadLine: "2022-07-25T13:00",
+      done: false,
+    },
+    {
+      taskId: nanoid(),
+      name: "Task 2 Name",
+      description: "Task 2 Description",
+      setDate: " 7/10/2022",
+      deadLine: "2022-07-28T13:00",
+      done: false,
+    },
+    {
+      taskId: nanoid(),
+      name: "Task 3 Name",
+      description: "Task 3 Description",
+      setDate: " 7/20/2022",
+      deadLine: "2022-07-30T13:00",
+      done: false,
+    },
+  ]);
+
+  function getSelectedTaskIndex(id) {
+    // Get index of object with specific value in array
+    const index = taskList.findIndex((item) => item.taskId === id);
+    return index;
+  }
+
+  function toggleTaskDoneFunction(id) {
+    const taskIndex = getSelectedTaskIndex(id);
+    // get the previous state
+    console.log(taskList[taskIndex].done);
+    let prev = taskList[taskIndex].done;
+    // update with the opposite of it
+    taskList[taskIndex].done = !prev;
+  }
+
   return (
     <>
       <div className="row my-4">
@@ -45,7 +89,7 @@ function ReminderPage() {
           </div>
         </div>
       </div>
-
+      {/* 
       <div className="container my-4">
         <div className="row">
           <div className="col-6">
@@ -59,7 +103,7 @@ function ReminderPage() {
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
       <div className="row">
         <div className="col-12">
           <div className="main-card full-main-card">
@@ -68,48 +112,7 @@ function ReminderPage() {
             </b>
             <div className="row">
               <div className="col-md-6">
-                <div className="form-div dashboard-form">
-                  <form action="">
-                    <div className="input-div">
-                      <label htmlFor="task-name">
-                        <FaUserTag />
-                      </label>
-                      <input
-                        type="text"
-                        id="task-name"
-                        placeholder="Task Name"
-                      />
-                    </div>
-                    <div className="textarea-div">
-                      <label htmlFor="password" className="text-areal-label">
-                        <FaStickyNote />
-                      </label>
-                      <textarea
-                        name=""
-                        cols="30"
-                        rows="3"
-                        placeholder="Task Discretion"
-                      ></textarea>
-                    </div>
-                    <sub className="m-0">Task Dead Line</sub>
-                    <div className="input-div mt-0">
-                      <label htmlFor="task-name">
-                        <FaCalendarDay />
-                      </label>
-                      <input
-                        type="datetime-local"
-                        id="task-name"
-                        placeholder="Dead Line"
-                      />
-                    </div>
-                    <div>
-                      <button className="form-btn">
-                        <FaPaperPlane />
-                        Add Task
-                      </button>
-                    </div>
-                  </form>
-                </div>
+                <TaskFormComponent />
               </div>
               <div className="col-md-6">
                 <b className="d-block my-2">
@@ -119,38 +122,10 @@ function ReminderPage() {
                 </b>
                 <div className="reminder-task-div">
                   <ul>
-                    <li className="task-list">
-                      <b>Task Name</b>
-                      <p>Task Discretion</p>
-                      <span className="d-flex justify-content-between">
-                        <i>
-                          <b>set :</b> Date
-                        </i>
-                        <i>
-                          <b>dead line :</b> Date
-                        </i>
-                      </span>
-                      <div className="icon-div">
-                        <FaCheckDouble />
-                        <FaTrash />
-                      </div>
-                    </li>
-                    <li className="task-list">
-                      <b>Task Name</b>
-                      <p>Task Discretion</p>
-                      <span className="d-flex justify-content-between">
-                        <i>
-                          <b>set :</b> Date
-                        </i>
-                        <i>
-                          <b>dead line :</b> Date
-                        </i>
-                      </span>
-                      <div className="icon-div">
-                        <FaCheckDouble />
-                        <FaTrash />
-                      </div>
-                    </li>
+                    <MapTaskComponent
+                      taskList={taskList}
+                      toggleTaskDoneFunction={toggleTaskDoneFunction}
+                    />
                   </ul>
                 </div>
               </div>
@@ -187,7 +162,7 @@ function ReminderPage() {
                         name=""
                         cols="30"
                         rows="3"
-                        placeholder="Event Discretion"
+                        placeholder="Event description"
                       ></textarea>
                     </div>
                     <sub className="m-0">Event Date&Time</sub>
@@ -220,7 +195,7 @@ function ReminderPage() {
                   <ul>
                     <li className="event-list">
                       <b>Event Name</b>
-                      <p>Event Discretion</p>
+                      <p>Event description</p>
                       <span className="d-flex justify-content-between">
                         {/* <!-- <i><b>set :</b> Date</i> --> */}
                         <i>
@@ -235,7 +210,7 @@ function ReminderPage() {
                     </li>
                     <li className="event-list">
                       <b>Event Name</b>
-                      <p>Event Discretion</p>
+                      <p>Event description</p>
                       <span className="d-flex justify-content-between">
                         {/* <!-- <i><b>set :</b> Date</i> --> */}
                         <i>
@@ -260,3 +235,74 @@ function ReminderPage() {
 }
 
 export default ReminderPage;
+
+export const TaskFormComponent = () => {
+  return (
+    <div className="form-div dashboard-form">
+      <form action="">
+        <div className="input-div">
+          <label htmlFor="task-name">
+            <FaUserTag />
+          </label>
+          <input type="text" id="task-name" placeholder="Task Name" />
+        </div>
+        <div className="textarea-div">
+          <label htmlFor="password" className="text-areal-label">
+            <FaStickyNote />
+          </label>
+          <textarea
+            name=""
+            cols="30"
+            rows="3"
+            placeholder="Task description"
+          ></textarea>
+        </div>
+        <sub className="m-0">Task Dead Line</sub>
+        <div className="input-div mt-0">
+          <label htmlFor="task-name">
+            <FaCalendarDay />
+          </label>
+          <input type="datetime-local" id="task-name" placeholder="Dead Line" />
+        </div>
+        <div>
+          <button className="form-btn">
+            <FaPaperPlane />
+            Add Task
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export const MapTaskComponent = ({ taskList, toggleTaskDoneFunction }) => {
+  const { editDateFunction } = useContext(UserContext);
+
+  return (
+    <>
+      {taskList.map((task) => {
+        const { taskId, name, description, setDate, deadLine, done } = task;
+
+        return (
+          <li className={`task-list ${done ? "completed" : null}`} key={taskId}>
+            <b>{name}</b>
+            <p>{description}</p>
+            <span className="d-flex justify-content-between flex-wrap">
+              <i>
+                <b>set :</b>
+                {setDate}
+              </i>
+              <i>
+                <b>dead line :</b> {editDateFunction(deadLine)}
+              </i>
+            </span>
+            <div className="icon-div">
+              <FaCheckDouble onClick={() => toggleTaskDoneFunction(taskId)} />
+              <FaTrash />
+            </div>
+          </li>
+        );
+      })}
+    </>
+  );
+};
